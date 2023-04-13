2b110381d0436155a836f24ba649d2ea12fc698b
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2023-04-13 12:51:23 +0400

    return all file

--- Assets/Scripts/UI/GameScreenMenu.cs
+++ Assets/Scripts/UI/GameScreenMenu.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using IJunior.TypedScenes;
 using TMPro;
 
 [RequireComponent(typeof(CanvasGroup))]
@@ -88,13 +87,11 @@ public class GameScreenMenu : MonoBehaviour
 
         if (_desertLevel != 0)
         {
-            //_level2Button.interactable = true;
             _blockLvlDesert.SetActive(false);
         }
 
         if (_winterLevel != 0)
         {
-            //_level3Button.interactable = true;
             _blockLvlWinter.SetActive(false);
         }
 
